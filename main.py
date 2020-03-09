635345ed4a3850693c97a81778a01ffe9d0cf364
Thomas PEUZIAT <17217655+thomas-peuziat@users.noreply.github.com>
2020-03-09 11:38:04 +0100

    [~] ajout commentaires, gitignore models

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ if __name__ == '__main__':
     start = time.time()
     pv_dataset_path = os.path.join("datasets", 'ninja_pv_47.2846_-1.5167_corrected.csv')
     wind_dataset_path = os.path.join("datasets", 'ninja_wind_47.2846_-1.5167_corrected.csv')
+
+    # Caracteristiques installation en kW
     MAX_POWER_WIND = 2
     MAX_POWER_SOLAR = 10
 
+    # Generation des fichiers .joblib
+    TRAINING = True
+
     dataset = data.init_dataset(pv_dataset_path=pv_dataset_path, wind_dataset_path=wind_dataset_path)
 
-    # model_wind_lr, model_pv_lr = data.fit_linear_regression(dataset=dataset)
-    # model_wind_gb, model_pv_gb = data.fit_gradient_boosting_regression(dataset=dataset)
+    if TRAINING:
+        # data.fit_linear_regression(dataset=dataset)
+        data.fit_gradient_boosting_regression(dataset=dataset)
 
     # model_wind_lr, model_pv_lr = data.load_model(os.path.join('models', 'model_wind_lr.joblib'),
     #                                              os.path.join('models', 'model_pv_lr.joblib'))
@@ -30,9 +36,15 @@ if __name__ == '__main__':
     parser.write_output(predictions=predictions)
     
     end = time.time()
-    
+
+    print("----------")
     print("Prévisions météo :")
     pprint(daily_data)
-    print("\nPrédictions production :")
+    print("----------")
+    print("Puissance installation éolienne :", MAX_POWER_WIND, "kw")
+    print("Puissance installation solaire :", MAX_POWER_SOLAR, "kw")
+    print("----------")
+    print("Prédictions production :")
     pprint(predictions)
-    print("\nTemps de réponse :", end-start, "s")
+    print("----------")
+    print("Temps de réponse total:", end-start, "s")
